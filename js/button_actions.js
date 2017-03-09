5e25c967f301eba0720f90dd9ed29b097a1a36e9
sergio-roldan <sergio.roldan.gomez@gmail.com>
2017-03-09 01:17:23 +0100

    Load and Save files

--- js/button_actions.js
+++ js/button_actions.js
@@ -1,13 +1,28 @@
+/**
+ * Script with some functions to perform the necessary actions in the interaction with the user.
+ * 
+ * @author: Sergio Roldan
+ */
+
+
+/**
+ * Function that selects one from a set of local predefined example files and loads in into the editor
+ * @param file	The desired file
+ */
 function loadExample(file) {
-	var xmlhttp = new XMLHttpRequest();
-	var editor = getEditor();
 	
+	// Object to handle file loads
+	var xmlhttp = new XMLHttpRequest();
+	
+	// This event will be triggered once the file is loaded	
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
+			var editor = getEditor();
 			editor.setValue(this.responseText);
 		}
 	};
 	
+	// Open the desired file
 	switch (file) {
 		case 'new':
 		default:
@@ -53,3 +68,64 @@ function loadExample(file) {
 	
 	xmlhttp.send();
 }
+
+
+/**
+ * Function that is triggered when the user wants to load a file using the 'input' html object
+ * @param evt	The event of the 'input' object when it is changed (file selected)
+ */
+function onLoadFile(evt) {
+    
+	// Check API support
+	if (window.File && window.FileReader && window.FileList && window.Blob) {
+		// Get the last selected file
+		var file = evt.target.files[0];
+		if (file) {
+			
+			// Check the extension of the selected file
+			var fileName = file.name;
+			var fileExtension = fileName.split('.')[fileName.split('.').length - 1].toLowerCase();
+			if (fileExtension != 'ino') {
+				window.alert('The selected file must be a valid *.ino sketch!')
+				return;
+			} 
+			
+			// Read the file
+			var fr = new FileReader();
+			fr.onload = function(e) {
+				var contents = e.target.result;
+				var editor = getEditor();
+				editor.setValue(contents);
+				document.getElementById('editor-select').value = 'new';
+			}
+			
+			fr.readAsText(file);
+		}
+	}
+}
+
+/**
+ * Function that is triggered when the user wants to save the text in the code editor in a file
+ */
+function onSaveFile()
+{
+    var text = getEditor().getValue();
+    var blob = new Blob([text], {type:'text/plain'});
+    var fileName = 'CubeSketch.ino';
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileName;
+    downloadLink.innerHTML = 'Download File';
+    
+	if (window.webkitURL != null) {
+        downloadLink.href = window.webkitURL.createObjectURL(blob);
+    } else  {
+        downloadLink.href = window.URL.createObjectURL(blob);
+        downloadLink.style.display = 'none';
+        document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
+}
+
+  
